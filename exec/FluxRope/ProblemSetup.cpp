640e4c848bce366e9896fde6c4d27ceb12f3cffd
Ian May <imay1@ucsc.edu>
2023-02-02 15:31:35 -0800

    Added/modified more MHD example problems

---
+++ exec/FluxRope/ProblemSetup.cpp
@@ -0,0 +1,36 @@
+#include <array>
+
+#include "ProblemSetup.H"
+#include "BoundaryConditions_K.H"
+#include "physics/EquationTypes.H"
+
+// Names of variables inside solution files
+std::array<std::string, KFVM::NUM_VARS> KFVM::ProblemSetup::varName = {
+  "dens",
+  "momx",
+  "momy",
+  "momz",
+  "etot",
+  "magx",
+  "magy",
+  "magz",
+  "psi"};
+
+std::array<std::string, KFVM::NUM_AUX> KFVM::ProblemSetup::auxVarName = {
+  "velx",
+  "vely",
+  "velz",
+  "eint",
+  "pres",
+  "prsg",
+  "prsb"};
+
+// Boundary conditions in order west, east, south, north, bottom, top
+// Note: Always all 6, even in 2D. bottom/top ignored in 2D
+std::array<KFVM::BCType,6> KFVM::ProblemSetup::bcType = {
+  KFVM::BCType::periodic,
+  KFVM::BCType::periodic,
+  KFVM::BCType::reflecting,
+  KFVM::BCType::reflecting,
+  KFVM::BCType::periodic,
+  KFVM::BCType::periodic};
