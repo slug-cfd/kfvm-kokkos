e8abf461c7e5ebd99a220b8fb8526ec224884277
Ian May <imay1@ucsc.edu>
2023-09-06 12:23:17 -0700

    Added jet crossflow example to NS

---
+++ exec/NavierStokes/JetCrossFlow/ProblemSetup.cpp
@@ -0,0 +1,13 @@
+#include <array>
+
+#include "BoundaryConditions_K.H"
+#include "ProblemSetup.H"
+
+// Boundary conditions in order west, east, south, north, bottom, top
+// Note: Always all 6, even in 2D. bottom/top ignored in 2D
+std::array<KFVM::BCType, 6> KFVM::ProblemSetup::bcType = {
+    KFVM::BCType::outflow,    KFVM::BCType::outflow,  KFVM::BCType::user,
+    KFVM::BCType::reflecting, KFVM::BCType::periodic, KFVM::BCType::periodic};
+
+// Set whether this problem has source terms
+bool KFVM::ProblemSetup::haveSourceTerms = false;
