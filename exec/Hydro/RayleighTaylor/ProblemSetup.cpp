8ff4e4d72168e1880670cdc18141d5e22258f0c2
Ian May <imay1@ucsc.edu>
2023-10-10 12:00:14 -0700

    Removed initial time from all input files, added different ICs for 2d Rayleigh-Taylor.

--- exec/Hydro/RayleighTaylor/ProblemSetup.cpp
+++ exec/Hydro/RayleighTaylor/ProblemSetup.cpp
@@ -5,9 +5,15 @@
 
 // Boundary conditions in order west, east, south, north, bottom, top
 // Note: Always all 6, even in 2D. bottom/top ignored in 2D
+#if (SPACE_DIM == 2)
 std::array<KFVM::BCType, 6> KFVM::ProblemSetup::bcType = {
-    KFVM::BCType::periodic,   KFVM::BCType::periodic, KFVM::BCType::reflecting,
-    KFVM::BCType::reflecting, KFVM::BCType::periodic, KFVM::BCType::periodic};
+    KFVM::BCType::reflecting, KFVM::BCType::reflecting, KFVM::BCType::user,
+    KFVM::BCType::user,       KFVM::BCType::reflecting, KFVM::BCType::reflecting};
+#else
+std::array<KFVM::BCType, 6> KFVM::ProblemSetup::bcType = {
+    KFVM::BCType::reflecting, KFVM::BCType::reflecting, KFVM::BCType::reflecting,
+    KFVM::BCType::reflecting, KFVM::BCType::reflecting, KFVM::BCType::reflecting};
+#endif
 
 // Set whether this problem has source terms
 bool KFVM::ProblemSetup::haveSourceTerms = true;
